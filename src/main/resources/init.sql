45a76782c0aff93e75bc0add523b53fdeb91939a
Anna Fuller <annafuller@Reverend-Nat.local>
2016-01-31 12:39:45 -0800

    Move adventure game out

--- src/main/resources/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS peons ( name varchar(50), health int(500), money int(10000), location varchar(50));
-CREATE TABLE IF NOT EXISTS rooms ( name varchar(50), type varchar(50), north varchar(50),
-                            south varchar(50), west varchar(50), east varchar(50));
-CREATE TABLE IF NOT EXISTS crops (name varchar(50), health int(100));
-CREATE TABLE IF NOT EXISTS weapons (name varchar(50), damage int(100));
-
-
-
